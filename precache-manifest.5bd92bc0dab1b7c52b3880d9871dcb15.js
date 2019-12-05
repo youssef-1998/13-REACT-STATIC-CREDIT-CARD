4e8f8bfff0e1408c639d7be6704717b770e04834
youssef-1998 <bouzar26@gmail.com>
2019-12-05 14:00:39 +0100

    Updates

---
+++ precache-manifest.5bd92bc0dab1b7c52b3880d9871dcb15.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0683a549c0f3319373906833793c017",
+    "url": "/13-REACT-STATIC-CREDIT-CARD/index.html"
+  },
+  {
+    "revision": "cac86bdd9dfbfd0a2be1",
+    "url": "/13-REACT-STATIC-CREDIT-CARD/static/css/main.1054b5c4.chunk.css"
+  },
+  {
+    "revision": "dd371a62d4fb5776d6f0",
+    "url": "/13-REACT-STATIC-CREDIT-CARD/static/js/2.033b6ab0.chunk.js"
+  },
+  {
+    "revision": "cac86bdd9dfbfd0a2be1",
+    "url": "/13-REACT-STATIC-CREDIT-CARD/static/js/main.e0e14a7e.chunk.js"
+  },
+  {
+    "revision": "84e6acdfaa1381a235d9",
+    "url": "/13-REACT-STATIC-CREDIT-CARD/static/js/runtime-main.0517708f.js"
+  },
+  {
+    "revision": "d974f18a9c02d633b0e56cc434071b79",
+    "url": "/13-REACT-STATIC-CREDIT-CARD/static/media/BungeeHairline-Regular.d974f18a.ttf"
+  },
+  {
+    "revision": "1aff84911778a4de8c9ac27f1aab2396",
+    "url": "/13-REACT-STATIC-CREDIT-CARD/static/media/background.1aff8491.jpg"
+  },
+  {
+    "revision": "7cf0ee8b7a9e8287267792040650dc7a",
+    "url": "/13-REACT-STATIC-CREDIT-CARD/static/media/puce 1.7cf0ee8b.png"
+  },
+  {
+    "revision": "260255dd28304881a30a1717edd973ad",
+    "url": "/13-REACT-STATIC-CREDIT-CARD/static/media/visa.260255dd.jpeg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/13-REACT-STATIC-CREDIT-CARD/precache-manifest.5bd92bc0dab1b7c52b3880d9871dcb15.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/13-REACT-STATIC-CREDIT-CARD/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
